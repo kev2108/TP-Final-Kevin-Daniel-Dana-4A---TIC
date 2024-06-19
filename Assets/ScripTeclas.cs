76e32d98b8133d52558a11bb6e8a06c57f813096
kev2108 <kevindanieldana@gmail.com>
2024-06-19 09:24:01 -0300

    Commit Parte 1

---
+++ Assets/ScripTeclas.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScripTeclas : MonoBehaviour
+{
+    public GameObject[] objetos;
+    int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 1; i < objetos.Length; i++)
+        {
+            objetos[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            objetos[counter].SetActive(false);
+            counter--;
+            if (counter == -1)
+            {
+                counter = objetos.Length-1;
+            }
+            objetos[counter].SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            objetos[counter].SetActive(false);
+            counter++;
+            if (counter == objetos.Length)
+            {
+                counter = 0;
+            }
+            objetos[counter].SetActive(true);
+        }
+    }
+}
